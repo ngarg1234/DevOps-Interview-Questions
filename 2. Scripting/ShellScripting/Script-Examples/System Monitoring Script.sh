38638246ceab08f5179af27a1c51e06124f87382
Your Name <you@example.com>
2025-05-14 17:59:48 +0530

    LatestCode-14thMay

--- 2. Scripting/ShellScripting/Script-Examples/System Monitoring Script.sh
+++ 2. Scripting/ShellScripting/Script-Examples/System Monitoring Script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cpu_threshold=90
 mem_threshold=90
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-mem_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+cpu_usage=$(echo "100 - $cpu_idle" | bc)
+
+# Memory usage (using grep and awk)
+mem_usage=$(free | grep Mem | awk '{printf "%.2f", $3/$2 * 100}')
 if (( $(echo "$cpu_usage > $cpu_threshold" | bc -l) )) || (( $(echo "$mem_usage > $mem_threshold" | bc -l) )); then
     echo "High CPU or memory usage detected!"
     # Send alert here
